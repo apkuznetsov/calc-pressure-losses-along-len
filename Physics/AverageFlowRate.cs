33632ef679cdfbf627a53140640880ce32134b78
Andrey <kouznts@ya.ru>
2020-03-06 14:32:07 +0400

    рефакторинг AverageFlowRate

--- Physics/AverageFlowRate.cs
+++ Physics/AverageFlowRate.cs
@@ -4,34 +4,13 @@ namespace calc_pressure_losses_along_len.Physics
 {
     public class AverageFlowRate
     {
-        private readonly double pipelineFluidFlow;
-        private readonly double flowSectionArea;
-
         private readonly double val;
 
         public AverageFlowRate(double pipelineFluidFlow, double pipelineInnerDiameter)
         {
-            this.pipelineFluidFlow = pipelineFluidFlow;
-            flowSectionArea =
-                Math.PI * Math.Pow(pipelineInnerDiameter, 2) / 2;
+            double flowSectionArea = Math.PI * Math.Pow(pipelineInnerDiameter, 2) / 2;
 
-            val = this.pipelineFluidFlow / flowSectionArea;
-        }
-
-        public double PipelineFluidFlow
-        {
-            get
-            {
-                return pipelineFluidFlow;
-            }
-        }
-
-        public double FlowSectionArea
-        {
-            get
-            {
-                return flowSectionArea;
-            }
+            val = pipelineFluidFlow / flowSectionArea;
         }
 
         public double Value
