e48e7792bfeceb3bbe8c1b4bd9e0905fe0938e04
Andrey <kouznts@ya.ru>
2020-03-06 14:48:05 +0400

    рефакторинг PressureLoss

--- Physics/PressureLoss.cs
+++ Physics/PressureLoss.cs
@@ -4,72 +4,20 @@ namespace calc_pressure_losses_along_len.Physics
 {
     public class PressureLoss
     {
-        private readonly double hydraulicPressureCoefficient;
-        private readonly double pipelineLength;
-        private readonly double pipelineInnerDiameter;
-        private readonly double averageFlowRate;
-        private readonly double fluidDensity;
-
         private readonly double val;
 
         public PressureLoss(
-            double hydraulicPressureCoefficient,
+            HydraulicFrictionCoefficient hydraulicFrictionCoefficient,
             double pipelineLength,
             double pipelineInnerDiameter,
-            double averageFlowRate,
+            AverageFlowRate averageFlowRate,
             double fluidDensity)
         {
-            this.hydraulicPressureCoefficient = hydraulicPressureCoefficient;
-            this.pipelineLength = pipelineLength;
-            this.pipelineInnerDiameter = pipelineInnerDiameter;
-            this.averageFlowRate = averageFlowRate;
-            this.fluidDensity = fluidDensity;
-
             val =
-                this.hydraulicPressureCoefficient *
-                (this.pipelineLength / this.pipelineInnerDiameter) *
-                (Math.Pow(this.averageFlowRate, 2) / 2) *
-                this.fluidDensity;
-        }
-
-        public double HydraulicPressureCoefficient
-        {
-            get
-            {
-                return hydraulicPressureCoefficient;
-            }
-        }
-
-        public double PipelineLength
-        {
-            get
-            {
-                return pipelineLength;
-            }
-        }
-
-        public double PipelineInnerDiameter
-        {
-            get
-            {
-                return pipelineInnerDiameter;
-            }
-        }
-
-        public double AverageFlowRate
-        {
-            get
-            {
-                return averageFlowRate;
-            }
-        }
-
-        public double FluidDensity
-        {
-            get
-            {
-                return fluidDensity;
-            }
+                hydraulicFrictionCoefficient.Value *
+                (pipelineLength / pipelineInnerDiameter) *
+                (Math.Pow(averageFlowRate.Value, 2) / 2) *
+                fluidDensity;
         }
 
         public double Value
