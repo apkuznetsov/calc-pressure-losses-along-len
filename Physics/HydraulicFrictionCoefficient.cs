a7fea9b52d83e25a211be3d32fa7078c4ed5204c
Andrey <kouznts@ya.ru>
2020-03-06 11:14:42 +0400

    класс коэффициента гидравлического трения

---
+++ Physics/HydraulicFrictionCoefficient.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace calc_pressure_losses_along_len.Physics
+{
+    public class HydraulicFrictionCoefficient
+    {
+        public static readonly double MaxReynoldsNumberForLaminarFlow = 2000;
+
+        public static readonly double MinReynoldsNumberForTransientFlow = 2000;
+        public static readonly double MaxReynoldsNumberForTransientFlow = 4000;
+
+        public static readonly double MinReynoldsNumberForTurbulenFlow = 4000;
+
+        private readonly ReynoldsNumber reynoldsNumber;
+        private readonly double equivalentRoughness;
+        private readonly double pipelineInnerDiameter;
+
+        private readonly double val;
+
+        public HydraulicFrictionCoefficient(
+            ReynoldsNumber reynoldsNumber,
+            double equivalentRoughness,
+            double pipelineInnerDiameter)
+        {
+            this.reynoldsNumber = reynoldsNumber;
+            this.equivalentRoughness = equivalentRoughness;
+            this.pipelineInnerDiameter = pipelineInnerDiameter;
+
+            if (IsLaminarFlow())
+                val = CalcLaminarFlowReynoldsNumber();
+            else if (IsTransientFlow())
+                val = CalcTransientFlow();
+            else if (IsTurbulentFlow())
+                val = CalcTurbulentFlowReynoldsNumber();
+            else
+                throw new ArgumentException();
+        }
+
+        private double CalcLaminarFlowReynoldsNumber()
+        {
+            return 64 / ReynoldsNumber;
+        }
+
+        private double CalcTurbulentFlowReynoldsNumber()
+        {
+            return
+                0.11 *
+                Math.Pow(
+                    ((equivalentRoughness / pipelineInnerDiameter) + (68 / ReynoldsNumber)),
+                    0.25);
+        }
+
+        private double CalcTransientFlow()
+        {
+            double arg = (Math.PI / 2) * (ReynoldsNumber / 2000 - 1);
+            double sin = Math.Sin(arg);
+            double ksi = Math.Pow(sin, 2);
+
+            return (1 - ksi) * CalcLaminarFlowReynoldsNumber() + ksi * CalcTurbulentFlowReynoldsNumber();
+        }
+
+        private double ReynoldsNumber
+        {
+            get
+            {
+                return reynoldsNumber.Value;
+            }
+        }
+
+        public double Value
+        {
+            get
+            {
+                return val;
+            }
+        }
+
+        public bool IsLaminarFlow()
+        {
+            return ReynoldsNumber <= MaxReynoldsNumberForLaminarFlow;
+        }
+
+        public bool IsTransientFlow()
+        {
+            return (ReynoldsNumber >= MinReynoldsNumberForTransientFlow) && (ReynoldsNumber <= MaxReynoldsNumberForTransientFlow);
+        }
+
+        public bool IsTurbulentFlow()
+        {
+            return ReynoldsNumber >= MinReynoldsNumberForTurbulenFlow;
+        }
+    }
+}
--- Physics/ReynoldsNumber.cs
+++ Physics/ReynoldsNumber.cs
@@ -41,7 +41,7 @@
             }
         }
 
-        private double Value
+        public double Value
         {
             get
             {
