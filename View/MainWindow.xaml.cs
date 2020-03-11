a45b6269979260527bc3bb2cdc646e561f2642d4
Andrey <kouznts@ya.ru>
2020-03-11 21:27:40 +0400

    переместить представление в папку View

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace calc_pressure_losses_along_len.View
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
