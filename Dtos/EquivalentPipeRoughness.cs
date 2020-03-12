206275cbed8fcafca2af4d54c6e9c4046b95c080
Andrey <kouznts@ya.ru>
2020-03-12 20:13:11 +0400

    DTO для БД

---
+++ Dtos/EquivalentPipeRoughness.cs
@@ -0,0 +1,11 @@
+﻿namespace calc_pressure_losses_along_len.Dtos
+{
+    public class EquivalentPipeRoughness
+    {
+        public int Id { get; set; }
+
+        public string PipeName { get; set; }
+
+        public double RoughnessValue { get; set; }
+    }
+}
---
+++ Dtos/KinematicViscosityCoefficients.cs
@@ -0,0 +1,19 @@
+﻿namespace calc_pressure_losses_along_len.Dtos
+{
+    public class KinematicViscosityCoefficients
+    {
+        public int Id { get; set; }
+
+        public string SubstanceName { get; set; }
+
+        public double ValueAt0Celsius { get; set; }
+
+        public double ValueAt20Celsius { get; set; }
+
+        public double ValueAt50Celsius { get; set; }
+
+        public double ValueAt70Celsius { get; set; }
+
+        public double ValueAt100Celsius { get; set; }
+    }
+}
