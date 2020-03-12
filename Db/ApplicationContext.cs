02ff083987fda64b8606d9d588125c3e84b5e02d
Andrey <kouznts@ya.ru>
2020-03-12 20:34:49 +0400

    контекст для БД

---
+++ Db/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using calc_pressure_losses_along_len.Dtos;
+using System.Data.Entity;
+
+namespace calc_pressure_losses_along_len.Db
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("DefaultConnection") { }
+
+        public DbSet<EquivalentPipeRoughness> Roughness { get; set; }
+
+        public DbSet<KinematicViscosityCoefficients> Coefficients { get; set; }
+    }
+}
