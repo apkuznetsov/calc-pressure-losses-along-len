8fec1292a48bf6cb901a85de3fac409ad26f4119
Andrey <kouznts@ya.ru>
2020-03-12 14:34:20 +0400

    реализация ICommand

---
+++ Commands/DelegateCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace calc_pressure_losses_along_len.Commands
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public DelegateCommand(Action<object> executeAction) : this(executeAction, null)
+        {
+            execute = executeAction;
+        }
+
+        public DelegateCommand(Action<object> executeAction, Func<object, bool> canExecuteFunc)
+        {
+            execute = executeAction;
+            canExecute = canExecuteFunc;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute != null)
+                return canExecute(parameter);
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (execute != null)
+                execute(parameter);
+        }
+    }
+}
