41db8408a791834abcad5de7e6cb178b7b749cc5
Andrey <kouznts@ya.ru>
2020-03-14 13:27:28 +0400

    обработчик KvcDialog

---
+++ View/KvcDialog.xaml.cs
@@ -0,0 +1,23 @@
+﻿using calc_pressure_losses_along_len.Dtos;
+using System.Windows;
+
+namespace calc_pressure_losses_along_len.View
+{
+    public partial class KvcDialog : Window
+    {
+        public KinematicViscosityCoefficient KinematicViscosityCoefficient { get; set; }
+
+        public KvcDialog(KinematicViscosityCoefficient kvc)
+        {
+            InitializeComponent();
+
+            KinematicViscosityCoefficient = kvc;
+            DataContext = KinematicViscosityCoefficient;
+        }
+
+        private void Click_Ok(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
