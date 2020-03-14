0b7af2c838e3dee406c3b6c01eeb7c2444972d39
Andrey <kouznts@ya.ru>
2020-03-14 11:40:16 +0400

    контекст данных для EPRWindow

---
+++ View/EquivalentPipeRoughnessWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using calc_pressure_losses_along_len.Dtos;
+using System.Windows;
+
+namespace calc_pressure_losses_along_len.View
+{
+    public partial class EquivalentPipeRoughnessWindow : Window
+    {
+        private EquivalentPipeRoughness EquivalentPipeRoughness { get; set; }
+
+        public EquivalentPipeRoughnessWindow(EquivalentPipeRoughness epr)
+        {
+            InitializeComponent();
+
+            EquivalentPipeRoughness = epr;
+            DataContext = EquivalentPipeRoughness;
+        }
+
+        private void Click_Ok(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
