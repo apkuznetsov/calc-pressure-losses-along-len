f53e0492f20fad054111d994356e1016b3cab123
Andrey <kouznts@ya.ru>
2020-03-14 09:32:06 +0400

    реализация INotifyPropertyChanged в DTO

--- Db/Dtos/EquivalentPipeRoughness.cs
+++ Db/Dtos/EquivalentPipeRoughness.cs
@@ -1,11 +1,46 @@
-﻿namespace calc_pressure_losses_along_len.Dtos
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace calc_pressure_losses_along_len.Dtos
 {
-    public class EquivalentPipeRoughness
+    public class EquivalentPipeRoughness : INotifyPropertyChanged
     {
+        private string pipeName;
+        private double roughnessValue;
+
         public int Id { get; set; }
 
-        public string PipeName { get; set; }
+        public string PipeName
+        {
+            get
+            {
+                return pipeName;
+            }
+            set
+            {
+                pipeName = value;
+                OnPropertyChanged(nameof(PipeName));
+            }
+        }
 
-        public double RoughnessValue { get; set; }
+        public double RoughnessValue
+        {
+            get
+            {
+                return roughnessValue;
+            }
+            set
+            {
+                roughnessValue = value;
+                OnPropertyChanged(nameof(RoughnessValue));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
--- Db/Dtos/KinematicViscosityCoefficients.cs
+++ Db/Dtos/KinematicViscosityCoefficients.cs
@@ -1,19 +1,102 @@
-﻿namespace calc_pressure_losses_along_len.Dtos
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace calc_pressure_losses_along_len.Dtos
 {
-    public class KinematicViscosityCoefficients
+    public class KinematicViscosityCoefficients : INotifyPropertyChanged
     {
+        private string substanceName;
+        private double valueAt0Celsius;
+        private double valueAt20Celsius;
+        private double valueAt50Celsius;
+        private double valueAt70Celsius;
+        private double valueAt100Celsius;
+
         public int Id { get; set; }
 
-        public string SubstanceName { get; set; }
+        public string SubstanceName
+        {
+            get
+            {
+                return substanceName;
+            }
+            set
+            {
+                substanceName = value;
+                OnPropertyChanged(nameof(SubstanceName));
+            }
+        }
 
-        public double ValueAt0Celsius { get; set; }
+        public double ValueAt0Celsius
+        {
+            get
+            {
+                return valueAt0Celsius;
+            }
+            set
+            {
+                valueAt0Celsius = value;
+                OnPropertyChanged(nameof(ValueAt0Celsius));
+            }
+        }
 
-        public double ValueAt20Celsius { get; set; }
+        public double ValueAt20Celsius
+        {
+            get
+            {
+                return valueAt20Celsius;
+            }
+            set
+            {
+                valueAt20Celsius = value;
+                OnPropertyChanged(nameof(ValueAt20Celsius));
+            }
+        }
 
-        public double ValueAt50Celsius { get; set; }
+        public double ValueAt50Celsius
+        {
+            get
+            {
+                return valueAt50Celsius;
+            }
+            set
+            {
+                valueAt50Celsius = value;
+                OnPropertyChanged(nameof(ValueAt50Celsius));
+            }
+        }
 
-        public double ValueAt70Celsius { get; set; }
+        public double ValueAt70Celsius
+        {
+            get
+            {
+                return valueAt70Celsius;
+            }
+            set
+            {
+                valueAt70Celsius = value;
+                OnPropertyChanged(nameof(ValueAt70Celsius));
+            }
+        }
 
-        public double ValueAt100Celsius { get; set; }
+        public double ValueAt100Celsius
+        {
+            get
+            {
+                return valueAt100Celsius;
+            }
+            set
+            {
+                valueAt100Celsius = value;
+                OnPropertyChanged(nameof(ValueAt100Celsius));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
